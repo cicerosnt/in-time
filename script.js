48a843eea35e5cd100fd50f52c6f4391dfedcf94
Cícero Santos <cicero.dsantos@outlook.com.br>
2026-05-27 21:36:11 -0300

    minimalista

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function updateClock() {
   document.getElementById('h').textContent = h;
   document.getElementById('m').textContent = m;
   document.getElementById('s').textContent = s;
+  updateGreeting(now);
+}
+
+function updateGreeting(now) {
+  const hour = now.getHours();
+  const greeting = hour < 12 ? 'Bom dia!' : hour < 18 ? 'Boa tarde!' : 'Boa noite!';
+  document.getElementById('hello').textContent = greeting;
 }
 
 setInterval(updateClock, 1000);
